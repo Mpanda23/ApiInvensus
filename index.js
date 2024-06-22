f91696d719654f5453666e9cc20ff0f42c5076ba
Mpanda23 <82336217+Mpanda23@users.noreply.github.com>
2024-06-22 18:28:03 -0500

    Segundo Commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const cors = require('cors');
 require('dotenv').config();
 
