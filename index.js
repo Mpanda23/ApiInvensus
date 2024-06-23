f315bf2adfef8b6e2fa5ae5a2a94f82c15ecc2c8
Mpanda23 <82336217+Mpanda23@users.noreply.github.com>
2024-06-22 19:55:57 -0500

    otro commit

--- index.js
+++ index.js
@@ -18,15 +18,14 @@ app.use(bodyParser.json());
 
 const PUERTO = 3000;
 
-const conexion = mysql.createConnection(
-    {
-        host: process.env.DB_HOST,
-        database: process.env.DB_DATABASE,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        port: process.env.DB_PORT
-    }
-);
+const conexion = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT
+});
+
 
 app.listen(PUERTO, () => {
     console.log(`Servidor corriendo en el puerto ${PUERTO}`);
